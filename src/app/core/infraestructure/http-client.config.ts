4baf1b12c3ebeca513b64a2a9e3d63bf38993d1f
Diego <diegomedina04@outlook.com>
2025-05-15 10:58:49 -0500

    cositas mejoradas

--- src/app/core/infraestructure/http-client.config.ts
+++ src/app/core/infraestructure/http-client.config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const api = axios.create({
-  baseURL: 'http://10.1.1.216:5000/api',
+  baseURL: 'http://10.1.1.218:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
